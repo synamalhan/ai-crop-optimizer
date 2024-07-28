f864798b28e2505b2e243494d693d64ba351b31c
synamalhan <synamalhan22@gmail.com>
2024-07-28 16:13:11 +0530

    image path

--- pages/home.py
+++ pages/home.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import os
+from PIL import Image
+
 
 # from streamlit_gsheets import GSheetsConnection
 
 
 c1, c2, c3 = st.columns([1, 6, 1])
 
-c1.image(os.path.join(os.path.dirname(__file__), "../assets/logo.png"), width=100)
+# Define the image path
+image_path = os.path.join(os.path.dirname(__file__), "../assets/logo.png")
+
+# Check if the image file exists
+if os.path.exists(image_path):
+    try:
+        # Open the image
+        img = Image.open(image_path)
+        c1.image(img, use_column_width=True)
+    except Exception as e:
+        st.error(f"Error loading image: {e}")
+else:
+    st.error("Image file not found.")
+
 c2.markdown("<h1 style='text-align: center;'>CropGenius</h1>", unsafe_allow_html=True)
 
 c1, c2, c3 = st.columns(3)
