7f5f978cedaee0513d53ac7168d6d79927cd4002
synamalhan <synamalhan22@gmail.com>
2024-07-28 16:10:50 +0530

    Image path

--- pages/home.py
+++ pages/home.py
@@ -1,14 +1,12 @@
 import streamlit as st
+import os
 
 # from streamlit_gsheets import GSheetsConnection
 
 
 c1, c2, c3 = st.columns([1, 6, 1])
 
-c1.image(
-    "https://github.com/synamalhan/ai-crop-optimizer/blob/197954692f89d54c95c5a1ee8a6fcf88f98a8c92/assets/logo.png",
-    width=100,
-)
+c1.image(os.path.join(os.path.dirname(__file__), "../assets/logo.png"), width=100)
 c2.markdown("<h1 style='text-align: center;'>CropGenius</h1>", unsafe_allow_html=True)
 
 c1, c2, c3 = st.columns(3)
